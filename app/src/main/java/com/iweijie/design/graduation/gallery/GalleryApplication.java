8af81c8e2160747c1df994ab54c2a05b33685f53
liweijieok <1182837437@qq.com>
2016-05-08 15:55:49 +0800

    init pro

---
+++ app/src/main/java/com/iweijie/design/graduation/gallery/GalleryApplication.java
@@ -0,0 +1,13 @@
+package com.iweijie.design.graduation.gallery;
+
+import android.app.Application;
+
+/**
+ * Created by liweijie on 2016/5/8.
+ */
+public class GalleryApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/iweijie/design/graduation/gallery/GalleryMainActivity.java
@@ -0,0 +1,13 @@
+package com.iweijie.design.graduation.gallery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GalleryMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
